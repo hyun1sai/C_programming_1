6b230534cbbc3a00e643be853e502282a9919f1e
최현성 <107556736+hyun1sai@users.noreply.github.com>
2022-09-14 16:55:59 +0900

    Add files via upload

---
+++ p33.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+	int num1 = 0xA7, num2 = 0x43, num3 = 032, num4 = 024;
+	printf("0x47의 값: %x\n", num1);
+	printf("0x43의 값: %d\n", num2);
+	printf("032의 값: %d\n", num3);
+	printf("024의 값: %d\n", num4);
+	printf("%d-%d=%d\n", num1,num2,num1 - num2);
+	printf("%d+%d=%d\n", num3,num4,num3 + num4);
+	return 0;
+
+
+
+}
